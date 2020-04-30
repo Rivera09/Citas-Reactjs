eb8b6dded23a2d8518903a0e08a239b04beba49e
Jorge Rivera <jorgeman201@yahoo.com>
2020-04-30 14:11:46 -0600

    Proyecto terminado.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { Fragment,useState } from 'react';
-import Formulario from './components/Formulario';
-import Cita from './components/Cita';
+import React, { Fragment, useState,useEffect } from "react";
+import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 
 function App() {
+
+  // Citas en el localstorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) citasIniciales=[];
   // Arreglo de citas
-  const [citas,guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState(citasIniciales);
+  // Use effect para realizar ciertas operaciones cuando el state cambia.
+  useEffect(()=>{
+    localStorage.setItem('citas',JSON.stringify(citas));
+  },[citas]);
 
   // Función que tome las citas actuales y agrege la nueva.
-  function crearCita(cita){
-    guardarCitas([...citas,cita]);
+  function crearCita(cita) {
+    guardarCitas([...citas, cita]);
+  }
+  // Función que elimina una cita por id
+  function eliminarCita(id) {
+    guardarCitas(citas.filter((cita) => cita.id !== id));
   }
   return (
     <Fragment>
@@ -17,17 +29,16 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="one-half column">
-            <Formulario
-              crearCita={crearCita}
-            />
+            <Formulario crearCita={crearCita} />
           </div>
           <div className="one-half column">
-            <h2>Administra tus citas</h2>
-            {citas.map(cita=>(
-              <Cita
-                key={cita.id}
-                cita={cita}
-              />
+            <h2>
+              {citas.length > 0
+                ? "Administra tus citas."
+                : "No hay citas."}
+            </h2>
+            {citas.map((cita) => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
             ))}
           </div>
         </div>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from "react";
 import shortid from 'shortid';
+import PropTypes from 'prop-types';
 
 const Formulario = ({crearCita}) => {
   // Crear state de citas
@@ -95,5 +96,9 @@ const Formulario = ({crearCita}) => {
     </Fragment>
   );
 };
+
+Formulario.propTypes = {
+  crearCita:PropTypes.func.isRequired
+}
 
 export default Formulario;
